de2283046460da93c4b63e98e3cf81147ae1a992
LAPTOP-7JI17SMK\Richard Brown Thomse <richard-sej@live.dk>
2017-12-07 17:49:13 +0100

    added the index design

--- Index.php
+++ Index.php
@@ -1,9 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<?php include 'header.php' ?>
+	<link rel="stylesheet" type="text/css" href="Stylesheet.css">
 	<title>MSPHOTO</title>
 </head>
-<body>
-Hello world!
+
+<body id="FrontBody">
+<!-- Nav bar -->
+<div id="FrontNavBar">
+	<ul id = "FrontNavBar">
+		<li><a class="ative" href=""> FORSIDE </a></li>	
+		<li><a href=""> GRAVID </a></li>	
+		<li><a href=""> BØRN / FAMILIE </a></li>	
+		<li><a href=""> BRYLLUP </a></li>
+		<li><a href=""> MODE </a></li>	
+		<li style="float: right;"><a href=""> ANDET </a></li>
+	</ul>
+</div>
+<!-- End of nav bar -->
+
+
+
+<!-- Billed slideshow body -->
+<div id="MiddleScreenText">
+	<img src="Billeder\Test.png">
+</div>
+<!-- End of slideshow body -->
+
+<!-- Info bar -->
+<div>
+	i think there is some text here, not sure tho.
+</div>
+<!-- End of Info bar -->
 </body>
+
+
+
+
+
+
+
+
+
+
+
 </html>
