779ed84a585aeecaf0e3fbe61491fccf81b58e39
Andreas Zacho <zacho.andreas@outlook.com>
2017-12-08 08:15:48 +0100

    Basic header

---
+++ Header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="Stylesheet.css">
+	<meta charset="utf-8">
+</head>
+<body oncontextmenu="return false;">
+<header>
+	<h1>Insæt en form for tekst eller noget her hvis det lyster...</h1>
+	<div class="navbar">
+		<div class="container">
+			<a href="#">Forside</a>
+			<a href="#">Gravid</a>
+			<a href="#">Børn/Familie</a>
+			<a href="#">Bryllup</a>
+			<a href="#">Mode</a>
+		<div class="dropdown">
+	    	<button class="dropbtn">Dropdown
+    	  	<i class="fa fa-caret-down"></i>
+    		</button>
+    		<div class="dropdown-content">
+    			<a href="#">Kontakt</a>
+    			<a href="#">Prisliste</a>
+    			<a href="#">Facebook</a>    		
+    		</div>
+ 		</div>
+ 		</div>
+	</div> 
+</header>
+</body>
+</html>
+
+<!-- 
+Forside.
+Gravid.
+Børn/ familie
+bryllup
+mode
+
+Andet som en dropdown
+} indhold i dropdown.
+	Kontakt
+	Prisliste
+	Facebook
+}
+-->
